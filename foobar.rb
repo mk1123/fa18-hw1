129db81e56ac2a1add627b873fc3c22e6007d793
Japjotanan <megapopnotaneck@gmail.com>
2018-09-20 01:24:31 -0700

    popper

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,6 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.collect{|i| i.to_i}.collect { |e| e+2 } .select { |e| e % 2 == 0 }.uniq.reject { |e| e >= 10 }.inject(0, :+)
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  return a.sort().collect { |e| e + 1 }  
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -48,5 +48,5 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
-end
+  return word.chars.map { |var| values[var.to_sym] }.inject(0, :+)
+  end
